902f13d2fb3c38233e055223aa21b0c2eeb70168
sachurkin <70898825+sachurkin@users.noreply.github.com>
2021-08-25 18:53:38 +0300

    Update 1,2.sql

--- 1,2.sql
+++ 1,2.sql
@@ -1,5 +1,9 @@
-SELECT name, count(promocodes.promocode_id) AS total
-FROM orders JOIN promocodes ON orders.promocode_id = promocodes.promocode_id
-GROUP BY name
-ORDER BY count(promocodes.promocode_id) DESC
-LIMIT 1
+select pilot_id
+from Pilots
+join Flights on pilot_id = first_pilot_id
+join Planes on Flights.plane_id = Planes.plane_id
+where cargo_flg = 0
+and flight_dt >= '2021/1/1'
+group by pilot_id
+order by sum(quantity) desc
+limit 10
